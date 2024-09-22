b8c8b8e783798d8a8bf1cda8a13509844aa6b316
Kristin198689 <kristin198689@gmail.com>
2024-09-22 18:05:53 +0200

    header-Home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -45,17 +45,20 @@
 //   );
 // }
 
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import styles from './Header.module.css';
-import Logo from '../../assets/icons/logo.svg';
-import cartIcon from '../../assets/icons/cart.svg';
+import styles from "./Header.module.css";
+import Logo from "../../assets/icons/logo.svg";
+import cartIcon from "../../assets/icons/cart.svg";
 
 export default function Header() {
   const cartItems = useSelector((state) => state.cart.items);
-  const cartItemsCount = cartItems.reduce((total, item) => total + item.quantity, 0);
+  const cartItemsCount = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
 
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -71,26 +74,34 @@ export default function Header() {
             <img src={Logo} alt="Logo" />
           </Link>
           <div
-            className={`${styles.burgerMenu} ${menuOpen ? styles.open : ''}`}
+            className={`${styles.burgerMenu} ${menuOpen ? styles.open : ""}`}
             onClick={toggleMenu}
           >
             <span className={styles.burgerLine}></span>
             <span className={styles.burgerLine}></span>
             <span className={styles.burgerLine}></span>
           </div>
-          <nav className={`${styles.navBlock} ${menuOpen ? styles.open : ''}`}>
+          <nav className={`${styles.navBlock} ${menuOpen ? styles.open : ""}`}>
             <ul>
               <li>
-                <Link to="/" className={styles.navLink}>Main Page</Link>
+                <Link to="/" className={styles.navLink}>
+                  Main Page
+                </Link>
               </li>
               <li>
-                <Link to="/categories" className={styles.navLink}>Categories</Link>
+                <Link to="/categories" className={styles.navLink}>
+                  Categories
+                </Link>
               </li>
               <li>
-                <Link to="/products" className={styles.navLink}>All Products</Link>
+                <Link to="/products" className={styles.navLink}>
+                  All Products
+                </Link>
               </li>
               <li>
-                <Link to="/discounted-products" className={styles.navLink}>All Sales</Link>
+                <Link to="/discounted-products" className={styles.navLink}>
+                  All Sales
+                </Link>
               </li>
             </ul>
           </nav>
